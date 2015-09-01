83af1f1fa601db325dd8496639666805c201a795
Sean Parsons <github@futurenotfound.com>
2015-09-01 08:58:35 +0100

    Base query data retrieval complete.

--- src/Data/WorldBank.hs
+++ src/Data/WorldBank.hs
@@ -1,11 +1,20 @@
 module Data.WorldBank (
-  getCountryInfo
+    pullAllInfo
+  , WBIncomeLevelInfo
+  , WBCountryInfo
+  , WBSourceInfo
+  , WBTopicInfo
+  , countriesUrl
+  , incomeLevelsUrl
+  , sourcesUrl
+  , topicsUrl
 ) where
 
 import Control.Monad
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Resource
-import Data.ByteString.Lazy hiding (putStrLn)
+import Data.ByteString.Lazy hiding (putStrLn, unpack)
+import Data.Foldable
 import Data.Text
 import Data.WorldBank.TH
 import Data.Conduit
@@ -13,6 +22,7 @@ import Data.Conduit.List
 import Data.Aeson
 import Data.Aeson.TH
 import Data.Aeson.Types
+import Data.Scientific
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
 import qualified Data.Vector as V
@@ -22,7 +32,20 @@ data WBPageInfo = WBPageInfo
                 , piPages   :: Int
                 } deriving (Eq, Show)
 
-$(deriveJSON defaultOptions{fieldLabelModifier = fieldTwiddle wbPageInfoFieldMappings} ''WBPageInfo)
+parseAsInt :: Value -> Parser Int
+parseAsInt (Number n) = case (floatingOrInteger n) of
+                                                      Right i   -> return i
+                                                      Left _    -> mzero
+parseAsInt (String t) = case (reads $ unpack t) of
+                                          [(i, _)]  -> return i
+                                          _         -> mzero
+parseAsInt _          = mzero
+
+instance FromJSON WBPageInfo where
+  parseJSON (Object v)  = WBPageInfo <$>
+                          ((v .: "page") >>= parseAsInt) <*>
+                          ((v .: "pages") >>= parseAsInt)
+  parseJSON _           = mzero
 
 data WBCountryInfo = WBCountryInfo
                    { ciName     :: Text
@@ -32,33 +55,79 @@ data WBCountryInfo = WBCountryInfo
 
 $(deriveJSON defaultOptions{fieldLabelModifier = fieldTwiddle wbCountryInfoFieldMappings} ''WBCountryInfo)
 
-data WBResultPair a = WBResultPair
-                    { rpPageInfo      :: WBPageInfo
-                    , rpCountryInfo   :: [WBCountryInfo]
-                    }
+data WBIncomeLevelInfo = WBIncomeLevelInfo
+                       { ilId     :: Text
+                       , ilValue  :: Text
+                       } deriving (Eq, Show)
 
-instance (FromJSON a) => FromJSON (WBResultPair a) where
+$(deriveJSON defaultOptions{fieldLabelModifier = fieldTwiddle wbIncomeLevelInfoFieldMappings} ''WBIncomeLevelInfo)
+
+data WBSourceInfo = WBSourceInfo
+                  { siId    :: Text
+                  , siName  :: Text
+                  } deriving (Eq, Show)
+
+$(deriveJSON defaultOptions{fieldLabelModifier = fieldTwiddle wbSourceInfoFieldMappings} ''WBSourceInfo)
+
+data WBLendingTypeInfo = WBLendingTypeInfo
+                       { ltId         :: Text
+                       , ltIso2Code   :: Text
+                       , ltValue      :: Text
+                       } deriving (Eq, Show)
+
+$(deriveJSON defaultOptions{fieldLabelModifier = fieldTwiddle wbLendingTypeInfoFieldMappings} ''WBLendingTypeInfo)
+
+data WBTopicInfo = WBTopicInfo
+                 { tiId           :: Text
+                 , tiValue        :: Text
+                 , tiSourceNote   :: Text
+                 } deriving (Eq, Show)
+
+$(deriveJSON defaultOptions{fieldLabelModifier = fieldTwiddle wbTopicInfoFieldMappings} ''WBTopicInfo)
+
+data WBResultPair a = WBResultPair
+                    { rpPageInfo  :: WBPageInfo
+                    , rpResults   :: [a]
+                    } deriving (Eq, Show)
+
+instance FromJSON a => FromJSON (WBResultPair a) where
   parseJSON v =
     let checkArray a = case V.toList a of
                                          [first, second]  -> WBResultPair <$> parseJSON first <*> parseJSON second
                                          _                -> typeMismatch "Invalid result pairing." v
     in  withArray "Expected array at top level." checkArray v
 
-decodeResult :: Response ByteString -> Maybe (WBResultPair WBCountryInfo)
-decodeResult response = decode' $ responseBody response
+decodeResult :: FromJSON a => Response ByteString -> Either String (WBResultPair a)
+decodeResult response = eitherDecode' $ responseBody response
 
-pullInfo :: Int -> Manager -> ConduitM () [WBCountryInfo] (ResourceT IO) ()
-pullInfo page manager = do
-  request <- liftIO $ parseUrl ("http://api.worldbank.org/countries/all?format=json&page=" ++ show page)
+pullInfo :: FromJSON a => (Int -> String) -> Int -> Manager -> ConduitM () a (ResourceT IO) ()
+pullInfo makeUrl page manager = do
+  request <- liftIO $ parseUrl $ makeUrl page
   response <- liftIO $ httpLbs request manager
-  let morePages v = yield v >> pullInfo (page + 1) manager
+  let unwind v = traverse_ yield v
+  let morePages v = unwind v >> pullInfo makeUrl (page + 1) manager
   case (decodeResult response) of
-                                            Just (WBResultPair WBPageInfo{..} countryInfo)  -> if piPage == piPages then yield countryInfo else morePages countryInfo
-                                            Nothing                                         -> liftIO $ fail "Couldn't decode response."
+                                  Right (WBResultPair WBPageInfo{..} result) -> if piPage == piPages then unwind result else morePages result
+                                  Left failure                               -> liftIO $ fail failure
 
-getCountryInfo :: IO [WBCountryInfo]
-getCountryInfo = do
-  fmap join $ runResourceT $ (bracketP (newManager tlsManagerSettings) closeManager (pullInfo 1) $$ consume)
+pullAllInfo :: FromJSON a => (Int -> String) -> IO [a]
+pullAllInfo makeUrl = do
+  runResourceT $ (bracketP (newManager tlsManagerSettings) (\_ -> return ()) (pullInfo makeUrl 1) $$ consume)
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+allQueriesUrl :: String -> Int -> String
+allQueriesUrl elementType page = "http://api.worldbank.org/v2/" ++ elementType ++ "/all?format=json&page=" ++ show page
+
+countriesUrl :: Int -> String
+countriesUrl = allQueriesUrl "countries"
+
+incomeLevelsUrl :: Int -> String
+incomeLevelsUrl = allQueriesUrl "incomeLevel"
+
+sourcesUrl :: Int -> String
+sourcesUrl = allQueriesUrl "sources"
+
+lendingTypesUrl :: Int -> String
+lendingTypesUrl = allQueriesUrl "lendingTypes"
+
+topicsUrl :: Int -> String
+topicsUrl = allQueriesUrl "topics"
--- src/Data/WorldBank/TH.hs
+++ src/Data/WorldBank/TH.hs
@@ -6,8 +6,23 @@ import Data.List
 fieldTwiddle :: [(String, String)] -> String -> String
 fieldTwiddle mappings name = fromMaybe name $ fmap snd $ find (\(key, _) -> key == name) mappings
 
-wbPageInfoFieldMappings :: [(String, String)]
-wbPageInfoFieldMappings = [("piPage", "page"), ("piPages", "pages")]
+wbPageInfoIFieldMappings :: [(String, String)]
+wbPageInfoIFieldMappings = [("piiPage", "page"), ("piiPages", "pages")]
+
+wbPageInfoSFieldMappings :: [(String, String)]
+wbPageInfoSFieldMappings = [("pisPage", "page"), ("pisPages", "pages")]
 
 wbCountryInfoFieldMappings :: [(String, String)]
 wbCountryInfoFieldMappings = [("ciName", "name"), ("ciId", "id"), ("ciIso2Code", "iso2Code")]
+
+wbIncomeLevelInfoFieldMappings :: [(String, String)]
+wbIncomeLevelInfoFieldMappings = [("ilId", "id"), ("ilValue", "value")]
+
+wbSourceInfoFieldMappings :: [(String, String)]
+wbSourceInfoFieldMappings = [("siId", "id"), ("siName", "name")]
+
+wbLendingTypeInfoFieldMappings :: [(String, String)]
+wbLendingTypeInfoFieldMappings = [("ltId", "id"), ("ltIso2Code", "iso2code"), ("ltValue", "value")]
+
+wbTopicInfoFieldMappings :: [(String, String)]
+wbTopicInfoFieldMappings = [("tiId", "id"), ("tiValue", "value"), ("tiSourceNote", "sourceNote")]
